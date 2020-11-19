8446fd9d83ebab1659900f1aec15f02f7f9764e1
aa <756237340@qq.com>
2020-11-19 16:28:07 +0800

    添加System语句

--- demo1/src/student.java
+++ demo1/src/student.java
@@ -3,4 +3,8 @@ public class student {
     private String name;
     private  String clName;
     private  int banji;
+
+    public static void main(String[] args) {
+        System.out.println("你好啊！");
+    }
 }
